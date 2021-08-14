13fff2e54d25c7d255567ea34fb20142fd6fe024
tlg u20 <lebanhat95@gmail.com>
2021-08-14 07:11:57 -0700

    .

--- open_term.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#for i in `seq 1 4`;
-#  do
-#  gnome-terminal -- /bin/sh -c 'cd /home/que/Desktop/mymodule; exec bash'
-#done 
-gnome-terminal --working-directory=/home/que/Desktop/mymodule
-gnome-terminal --working-directory=/home/que/Desktop/mymodule
-gnome-terminal --working-directory=/home/que/Desktop/mymodule --geometry=170x25 -- /bin/sh -c 'dmesg -wH; exec bash'
--- run-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-insmod mmap.ko
-./kernel_modules/mmap.out /proc/lkmc_mmap 2>&1 1>/dev/null
-rmmod mmap.ko
